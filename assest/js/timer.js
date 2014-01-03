a69869e53040620f5e0f808ed64c48cadfcd94a1
Lax Chee <lax.chee@2359media.com>
2014-01-04 02:51:42 +0800

    lalala

--- assest/js/timer.js
+++ assest/js/timer.js
@@ -192,7 +192,7 @@ $(function()
         setTimeout(function(){alertClock()},5000);
     });
 
-    jQuery(".phone-center").bind('pinchclose',function(){
+    jQuery(".phone-center, .iphone").bind('pinchclose',function(){
     	laxClock();
     	setTimeout(function(){alertClock()},5000);
     });
