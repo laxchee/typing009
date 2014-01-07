433e2f795cbf26c158150fe4cb5095e81a135efe
Lax Chee <lax.chee@2359media.com>
2014-01-08 01:08:56 +0800

    asdas

--- form-to-email.php
+++ form-to-email.php
@@ -30,9 +30,6 @@ $to = "laxchee@hotmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: thank-you.html');
-
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
