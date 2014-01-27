cadaa1d5a99c679c69b65f7648134f91735877cb
Lax Chee <lax.chee@2359media.com>
2014-01-27 09:16:34 +0800

    aasd

--- assest/js/life.js
+++ assest/js/life.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    window.addEventListener('touchstart', function() {
+    var buffer = myContext.createBuffer(1, 1, 22050);
+    var source = myContext.createBufferSource();
+    source.buffer = buffer;
+
+    // connect to output (your speakers)
+    source.connect(myContext.destination);
+
+    // play the file
+    source.noteOn(0);
+
+    }, false);
     var device = navigator.userAgent.toLowerCase();
     var ios = device.match(/(iphone)/);
     if (ios) {
