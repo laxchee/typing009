4da92bf3572e3fd643f0cacf227743dbaa989d47
Lax Chee <lax.chee@2359media.com>
2014-02-01 18:09:40 +0800

    dsadas

--- assest/js/life.js
+++ assest/js/life.js
@@ -12,12 +12,21 @@ $(document).ready(function(){
     // Home Screen
     // --------------------------------------------------------
     var audio1 = document.getElementById('audio');
-    
+
     $('.iphone_cover').click(function(){
         $(this).fadeOut( 1300 );
         $('.holder').addClass('blurme2');
         // audio1.load();
         audio1.play();
     });
+
+    var button = $('.iphone_cover');
+    var audio = document.getElementById('audio');
+
+    var onClick = function() {
+        audio.play(); // audio will load and then play
+    };
+
+    button.addEventListener('click', onClick, false);
 
 });
